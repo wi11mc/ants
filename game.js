6988b1f7232991b99f3904ea7a9487813dc5a212
wi11mc <157196275+wi11mc@users.noreply.github.com>
2024-04-26 11:11:14 -1000

    added some more ant pics

--- game.js
+++ game.js
@@ -1,17 +1,21 @@
+// game.js
 const gameContainer = document.getElementById('game-container');
 let score = 0;
+
 function createAnt() {
     const ant = document.createElement('div');
     ant.className = 'ant';
     ant.addEventListener('click', () => {
         score++;
         ant.remove();
-        ant.style.left = `${Math.random() * 90}%`;
+    });
+    ant.style.left = `${Math.random() * 90}%`;
     ant.style.top = `${Math.random() * 90}%`;
     gameContainer.appendChild(ant);
 }
+
 function startGame() {
-    setInterval(createAnt, 1000);
+    setInterval(createAnt, 1000); // Create an ant every second
 }
 
 startGame();
