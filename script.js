057e4223bf787061d131ef58c55a1de64f77b322
wi11mc <157196275+wi11mc@users.noreply.github.com>
2024-05-04 12:28:50 -1000

    slideshow working

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-const facts = document.querySelectorAll('.fact');
-let currentFactIndex = 0;
+let slideIndex = 1;
+showSlides(slideIndex);
 
-function showNextFact() {
-    facts[currentFactIndex].classList.remove('visible');
-    currentFactIndex = (currentFactIndex + 1) % facts.length;
-    facts[currentFactIndex].classList.add('visible');
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-setInterval(showNextFact, 3000);
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+} 
